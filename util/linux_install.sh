1ca66c401cbae58fb767d2fce5be10a3aa2d8a06
dohq <dorastone@gmail.com>
2020-01-14 18:41:52 +0900

    [fix] yay instead pacman install bootloadhid

--- util/linux_install.sh
+++ util/linux_install.sh
@@ -96,6 +96,12 @@ elif grep ID /etc/os-release | grep -q 'arch\|manjaro'; then
 		unzip \
 		wget \
 		zip
+    type yay >/dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        yay -S bootloadhid
+    else
+        echo "!!!Prease manually install bootloadhid package from AUR!!!"
+    fi
 
 elif grep ID /etc/os-release | grep -q gentoo; then
 	echo "$GENTOO_WARNING" | fmt
