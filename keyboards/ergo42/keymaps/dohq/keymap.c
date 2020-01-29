46ace0e41817bba20da02c1bd2d90b44fcc1d681
dohq <dorastone@gmail.com>
2020-01-29 17:18:35 +0900

    [mod] polish my keymap

--- keyboards/ergo42/keymaps/dohq/keymap.c
+++ keyboards/ergo42/keymaps/dohq/keymap.c
@@ -10,37 +10,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * | END  | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |   [  |   ]  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | LGUI | LAlt |      |      |      |      | MO(2)|   | MO(1)|      |      |      |      |      |      |
+   * | LGUI | LAlt |      |      |      | Lsft | MO(2)|   | MO(1)| Rsft |      |      |      |      |   \  |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [0] = LAYOUT( \
-    KC_GESC, KC_TAB,  KC_Q,   KC_W,  KC_E,  KC_R,  KC_T,           KC_Y,         KC_U,  KC_I,    KC_O,   KC_P,    KC_MINS, KC_BSPC, \
-    KC_HOME, KC_RCTL, KC_A,   KC_S,  KC_D,  KC_F,  KC_G,           KC_H,         KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
-    KC_END,  KC_LSFT, KC_Z,   KC_X,  KC_C,  KC_V,  KC_B,           KC_N,         KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_LBRC, KC_RBRC, \
-    KC_LGUI, KC_LALT, KC_NO,  KC_NO, KC_NO, KC_NO, LT(2,KC_SPC),   LT(1,KC_SPC), KC_NO, KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_NO \
+    KC_ESC,  KC_TAB,  KC_Q,   KC_W,  KC_E,  KC_R,    KC_T,            KC_Y,         KC_U,  KC_I,     KC_O,   KC_P,    KC_MINS, KC_BSPC, \
+    KC_HOME, KC_RCTL, KC_A,   KC_S,  KC_D,  KC_F,    KC_G,            KC_H,         KC_J,  KC_K,     KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
+    KC_END,  KC_LSFT, KC_Z,   KC_X,  KC_C,  KC_V,    KC_B,            KC_N,         KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_LBRC, KC_RBRC, \
+    KC_LGUI, KC_LALT, KC_NO,  KC_NO, KC_NO, KC_LSFT, LT(2,KC_SPC),    LT(1,KC_SPC), KC_RSFT, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_BSLS \
   ),
 
   /* 1
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   `  |      |   1  |   2  |   3  |   4  |   5  |   |   6  |   7  |   8  |   9  |   0  |   =  | Bksp |
+   * | ESC  |      |   1  |   2  |   3  |   4  |   5  |   |   6  |   7  |   8  |   9  |   0  |   =  | Bksp |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | PGUP |      |      |      |      |      |      |   |      | END  | HOME |      |      |      | Enter|
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * | PGDN |      |      |      |      |      |      |   |      |      |   ,  |   .  |   /  |      |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * |      |      |      |      |      |      | MO(2)|   | MO(1)|      |      |      |      |      |      |
+   * |      |      |      |      |      |      | MO(2)|   | MO(1)|      |      |      |      |      |   `  |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [1] = LAYOUT( \
-    KC_GRV,  KC_NO, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,       KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_EQL, _______, \
-    KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO,   KC_END, KC_HOME, KC_NO,   KC_NO,   KC_NO,  _______, \
-    KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,      KC_NO,   KC_NO,  _______, _______, _______, KC_NO,  KC_NO, \
-    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______,    _______, KC_NO,  KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO \
+    _______, KC_NO, KC_1,  KC_2,  KC_3,  KC_4,    KC_5,       KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_EQL, _______, \
+    KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,      KC_NO,   KC_END, KC_HOME, KC_NO,   KC_NO,   KC_NO,  _______, \
+    KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,      KC_NO,   KC_NO,  _______, _______, _______, KC_NO,  KC_NO, \
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, _______, _______,    _______, _______,  KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_GRV \
   ),
 
   /* 2
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   \  |      |  F1  |  F2  |  F3  |  F4  |  F5  |   |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+   * | ESC  |      |  F1  |  F2  |  F3  |  F4  |  F5  |   |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |   | Left | Down |  UP  | Right|      |      | Enter|
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [2] = LAYOUT( \
-    KC_BSLS, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,    KC_F6,   KC_F7,   KC_F8, KC_F9,   KC_F10, KC_F11, KC_F12, \
-    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,  KC_NO,  _______, \
-    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO, \
-    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, _______,  _______, KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO \
+    _______, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,        KC_F6,   KC_F7,   KC_F8, KC_F9,   KC_F10, KC_F11, KC_F12, \
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,  KC_NO,  _______, \
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO,   KC_NO,   KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO, \
+    KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, _______, _______,    _______, _______, KC_NO, KC_NO,   KC_NO,  KC_NO,  KC_NO \
   )
 };
