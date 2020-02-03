7f745e5f9841376abfc5f24e893512d8bed9acdc
dohq <dorastone@gmail.com>
2020-02-03 15:46:45 +0900

    [mod] add delete key

--- keyboards/ergo42/keymaps/dohq/keymap.c
+++ keyboards/ergo42/keymaps/dohq/keymap.c
@@ -10,14 +10,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * | END  | LSft |   Z  |   X  |   C  |   V  |  B   |   |  N   |   M  |   ,  |   .  |   /  |   [  |   ]  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | LGUI | LAlt |      |      |      | Lsft | MO(2)|   | MO(1)| Rsft |      |      |      |      |   \  |
+   * | LGUI | LAlt |      |      |      | Lsft | MO(2)|   | MO(1)| Rsft |      |      |      | DEL  |   \  |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [0] = LAYOUT( \
     KC_ESC,  KC_TAB,  KC_Q,   KC_W,  KC_E,  KC_R,    KC_T,            KC_Y,         KC_U,  KC_I,     KC_O,   KC_P,    KC_MINS, KC_BSPC, \
     KC_HOME, KC_RCTL, KC_A,   KC_S,  KC_D,  KC_F,    KC_G,            KC_H,         KC_J,  KC_K,     KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
     KC_END,  KC_LSFT, KC_Z,   KC_X,  KC_C,  KC_V,    KC_B,            KC_N,         KC_M,  KC_COMM,  KC_DOT, KC_SLSH, KC_LBRC, KC_RBRC, \
-    KC_LGUI, KC_LALT, KC_NO,  KC_NO, KC_NO, KC_LSFT, LT(2,KC_SPC),    LT(1,KC_SPC), KC_RSFT, KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_BSLS \
+    KC_LGUI, KC_LALT, KC_NO,  KC_NO, KC_NO, KC_LSFT, LT(2,KC_SPC),    LT(1,KC_SPC), KC_RSFT, KC_NO,  KC_NO,  KC_NO,   KC_DEL,  KC_BSLS \
   ),
 
   /* 1
