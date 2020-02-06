eca49946296c7956f6fb559acad2dd212c1146e7
dohq <dorastone@gmail.com>
2020-02-06 15:17:42 +0900

    [add] my crkbd keymap

---
+++ keyboards/crkbd/keymaps/dohq/rules.mk
@@ -0,0 +1,13 @@
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+SLEEP_LED_ENABLE = yes      # Breathing sleep LED during USB suspend
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/glcdfont.c \
+		./lib/rgb_state_reader.c \
+		./lib/layer_state_reader.c \
+		./lib/logo_reader.c \
+		./lib/keylogger.c \
+		# ./lib/mode_icon_reader.c \
+		# ./lib/host_led_state_reader.c \
+		# ./lib/timelogger.c \
