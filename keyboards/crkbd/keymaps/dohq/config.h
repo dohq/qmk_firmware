db4f34d86ca9f134780397221eeced592d9185e1
dohq <dorastone@gmail.com>
2020-02-29 19:22:30 +0900

    [mod] enable SandS

--- keyboards/crkbd/keymaps/dohq/config.h
+++ keyboards/crkbd/keymaps/dohq/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL_PD2
 
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 200
 #define USB_MAX_POWER_CONSUMPTION 100
 
 #ifdef RGBLIGHT_ENABLE
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_ANIMATIONS
     #define RGBLED_NUM 27
     #define RGBLIGHT_LIMIT_VAL 100
-    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_HUE_STEP 5
     #define RGBLIGHT_SAT_STEP 17
     #define RGBLIGHT_VAL_STEP 17
 #endif
